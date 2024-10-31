70f231b573127b83b3331dd69be4eec455d20543
thangldhe186175 <thangldhe186175@fpt.edu.vn>
2024-10-31 20:04:53 +0700

    update

--- WpfApp/obj/Debug/net8.0-windows/EmployeeManagementWindow.g.i.cs
+++ WpfApp/obj/Debug/net8.0-windows/EmployeeManagementWindow.g.i.cs
@@ -213,7 +213,7 @@ namespace WpfApp {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfApp;component/employeemanagementwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfApp;V1.0.0.0;component/employeemanagementwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\EmployeeManagementWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
